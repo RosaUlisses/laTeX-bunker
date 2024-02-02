829f2e87e116bd57e5a61d5e295734d371c174b3
RosaUlisses <ulissao@proton.me>
2024-02-02 20:02:06 -0300

    add all the laTeX projects that are my overleaf to the repo

---
+++ projects/pdsI-pratical-work/bash.bash
@@ -0,0 +1,4 @@
+# Como rodar o TP
+
+gcc main2.c -o exec
+./exec
---
+++ projects/pdsI-pratical-work/input_reader.h
@@ -0,0 +1,7 @@
+moon_t read_moon();
+
+planet_t read_planet();
+
+solar_system_t read_solar_system();
+
+solar_system_t* read_solar_systems(int number_of_solar_systems);
